4356f4511c2e823269b9cff9104bb6c6838db0fc
NoisyDam <azuria.ee83@gmail.com>
2025-06-16 21:00:09 +0700

    menu sidebar

--- script.js
+++ script.js
@@ -4,5 +4,14 @@ const navbarNav = document.querySelector(".navbar-nav");
 
 //hamburger menu responsif
 document.querySelector("#hamburger-menu").onclick = () => {
-  navbarNav.classlist.toggle("active");
+  navbarNav.classList.toggle("active");
 };
+
+//klik diluar sidebar buat ilangin nav
+
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
